52e88908959dea3b7a596addb67563a020e4d071
Ketan Joshi <ketanjoshi1224@gmail.com>
2022-10-02 20:49:02 -0700

    Added my HTTP API for Assignment 3

--- server/views.py
+++ server/views.py
@@ -1,4 +1,8 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def test(response):
     return HttpResponse("Testing my first HTTP API for CS4800 Assignment 3")
+
+def ketan(request):
+    return HttpResponse("Hello from Ketan")

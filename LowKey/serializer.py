3eeca3410a05bdce1f7f08f5ce3e73f4efd5c96f
Ketan Joshi <ketanjoshi1224@gmail.com>
2022-10-11 21:03:31 -0700

    Added serializers for the DB models

--- LowKey/serializer.py
+++ LowKey/serializer.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from . models import *
+from models import *
 
 # Serializers are basically used to convert complex data to native 
 # Python datatypes that can then be easily rendered into 
@@ -8,3 +8,39 @@ class ReactSerializer(serializers.ModelSerializer):
     class Meta:
         model = React
         fields = ['name', 'detail']
+
+class ProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Profile
+        fields = ['profile_id', 
+        'name',
+        'phoneNumber',
+        'email',
+        'password',
+        'description']
+
+class ShopSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Shop
+        fields = ['profile_id',
+        'address',
+        'Category',
+        'link']
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['product_id',
+        'profile_id',
+        'productName',
+        'price',
+        'description']
+
+class ShopSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Shop
+        fields = ['product_id',
+        'profile_id',
+        'productName',
+        'price',
+        'description']

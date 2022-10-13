6d82f2a3e20eb1a59f2de9160e57b42a3bce17f7
Ketan Joshi <ketanjoshi1224@gmail.com>
2022-10-12 17:21:54 -0700

    Created the models (DB Tables) we will be using in project

--- LowKey/models.py
+++ LowKey/models.py
@@ -1,6 +1,38 @@
+from asyncio import constants
+from itertools import product
+from pydoc import describe
+from unicodedata import category
 from django.db import models
 
 # Create your models here.
 class React(models.Model):
     name = models.CharField(max_length=30)
     detail = models.CharField(max_length=500)
+
+class Profile(models.Model):
+    profile_id = models.IntegerField(primary_key=True)
+    name = models.TextField()
+    phone_number = models.CharField(max_length=12)
+    email = models.EmailField()
+    password = models.TextField(max_length=30)
+    description = models.TextField()
+
+class Shop(models.Model):
+    profile_id = models.OneToOneField(Profile, primary_key=True, on_delete=models.CASCADE)
+    address = models.TextField()
+    category = models.TextField()
+    link = models.URLField()
+
+class Product(models.Model):
+    profile_id = models.ForeignKey(Shop, on_delete=models.CASCADE)
+    product_id = models.PositiveIntegerField(primary_key=True)
+    product_name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    description = models.TextField()
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['profile_id', 'product_name'], name='unique_product_key'
+            )
+        ]

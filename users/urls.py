f97fc7cc4ac414a0a95b639777518129061d3c8a
Benjamin Ung <Benjaminung@cpp.edu>
2022-11-02 13:50:28 -0700

    api returns JWT tokens. add user documentation

--- users/urls.py
+++ users/urls.py
@@ -6,3 +6,28 @@ urlpatterns = [
     path('auth/', include('dj_rest_auth.urls')),
     path('auth/register/', include('dj_rest_auth.registration.urls'))
 ]
+
+
+'''
+/dj-rest-auth/login/ (POST)
+
+        username
+        email
+        password
+    returns JWT access token + refresh token + user details
+
+/dj-rest-auth/logout/ (POST)
+
+/dj-rest-auth/token/verify/ (POST)
+
+        token
+
+/dj-rest-auth/register/ (POST)
+
+        username
+        password1
+        password2
+        email
+
+    returns JWT access token + refresh token + user details
+'''

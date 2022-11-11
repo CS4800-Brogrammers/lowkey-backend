126d6fca5710c5af25688681ad62c4179cb79711
Ketan Joshi <ketanjoshi1224@gmail.com>
2022-11-11 00:22:50 -0800

    Fixed the DB to a stable version, had to erase the data but can be easily repopulated

--- LowKey/serializer.py
+++ LowKey/serializer.py
@@ -1,40 +1,26 @@
 from rest_framework import serializers
-from .models import React, Profile, Shop, Product
+from .models import *
 
 # Serializers are basically used to convert complex data to native 
 # Python datatypes that can then be easily rendered into 
 # JSON(Which we are going to use in React i.e. Client side). 
-class ReactSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = React
-        fields = ['name', 'detail']
-
-class ProfileSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Profile
-        fields = ['profile_id', 
-        'name',
-        'phone_number',
-        'email',
-        'password',
-        'description']
 
 class ShopSerializer(serializers.ModelSerializer):
     class Meta:
         model = Shop
-        fields = ['shop_id',
+        fields = [
+        'shop_id',
         'name',
         'address',
         'category',
-        'link']
+        'description']
 
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
-        fields = ['product_id',
-        'profile_id',
+        fields = [
+        'shop_id',
         'product_name',
         'price',
         'description',
-        'rating',
-        'shop']
+        'rating']
